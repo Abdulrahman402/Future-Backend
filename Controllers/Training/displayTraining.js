dc2dfdcc9195ab1e7a0cb356ca21b28732c1f5f6
Abdulrahman <abduwemoh@gmail.com>
2020-09-10 11:27:14 +0200

    5th commit

--- Controllers/Training/displayTraining.js
+++ Controllers/Training/displayTraining.js
@@ -2,7 +2,7 @@ const { Training } = require("../../Models/Training");
 
 exports.displayTraining = async function(req, res, next) {
   const courses = await Training.find({ _id: req.params.id }).populate(
-    "manager trainingManager learner courses"
+    "manager trainingManager learner mandatoryCourses optionalCourses"
   );
 
   res.send(courses);
